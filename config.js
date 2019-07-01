75f5dde8e348ec8ccc8d5d7948a5a643afd54d85
felipeasf <felipeasf@gmail.com>
2019-07-01 18:06:30 -0300

    initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    api_url: 'http://127.0.0.1:8888/v1/chain/get_info',
+    //use  / at the end of path
+    data_path_1: '/Users/felipe/Documents/nodeos/data/',
+    data_path_2: '/Users/felipe/Documents/nodeos/data/',
+    config_path_1: '/Users/felipe/Documents/nodeos/config/',
+    config_path_2: '/Users/felipe/Documents/nodeos/config/',
+    snap_dir_1:'/Users/felipe/Documents/nodeos/data/snapshots/',
+    snap_dir_2:'/Users/felipe/Documents/nodeos/data/snapshots/',
+    check_time: 5000, // seconds
+    auto_restart: true
+};
